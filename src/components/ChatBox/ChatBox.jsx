f5408f38626253573005f49b844e5f38de39fa71
Parmod Kumar <yparmodyadav@gmail.com>
2024-08-11 15:11:18 +0530

    Chat Page Chat Box nav and bottom part ui created

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,9 +1,24 @@
+import assets from "../../assets/assets"
 import "./ChatBox.css"
 
 const ChatBox = () => {
     return (
-        <>
-        </>
+        <div className="chat-box">
+            <div className="chat-user">
+                <img src={assets.profile_img} alt="" />
+                <p>Richard Sanford <img src={assets.green_dot} alt="" className="dot" /></p>
+                <img src={assets.help_icon} className="help" alt="" />
+            </div>
+
+            <div className="chat-input">
+                <input type="text" placeholder="Send a message" />
+                <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+                <label htmlFor="image">
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                <img src={assets.send_button} alt="" />
+            </div>
+        </div>
     )
 }
 
