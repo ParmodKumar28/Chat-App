8dc54cf035666cf51fb231287e6090ea786e79a8
Parmod Kumar <yparmodyadav@gmail.com>
2024-08-15 16:57:08 +0530

    Profile update and chataData feature added

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,80 @@
-import { createContext } from "react";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { createContext, useEffect, useState } from "react";
+import { auth, db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
-const AppContextProvider = ({ props }) => {
+const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState(null);
+    const [chatData, setChatData] = useState(null);
+
+    const loadUserData = async (uid) => {
+        try {
+            const userRef = doc(db, "users", uid);
+            const userSnap = await getDoc(userRef);
+            const userData = userSnap.data();
+            setUserData(userData);
+            console.log("working");
+
+            if (userData.avatar && userData.name) {
+                navigate("/chat");
+            } else {
+                navigate("/profile");
+            }
+
+            await updateDoc(userRef, {
+                lastSeen: Date.now(),
+            })
+
+
+            setInterval(async () => {
+                if (auth.chatUser) {
+                    console.log("working");
+
+                    await updateDoc(userRef, {
+                        lastSeen: Date.now(),
+                    })
+                }
+            }, 60000)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        if (userData) {
+            const chatRef = doc(db, "chats", userData.id);
+            const unSub = onSnapshot(chatRef, async (res) => {
+                const chatItems = res.data().chatData;
+                const tempData = [];
+                for (const item of chatItems) {
+                    const userRef = doc(db, "users", item.rId);
+                    const userSnap = await getDoc(userRef);
+                    const userData = userSnap.data();
+                    tempData.push({ ...item, userData })
+                }
+                setChatData(tempData.sort((a, b) => b.updatedAt - a.updateAdt));
+            });
+            return () => {
+                unSub();
+            }
+        }
+    }, [userData])
 
     const value = {
-
+        userData,
+        setUserData,
+        chatData,
+        setChatData,
+        loadUserData
     }
 
     return (
         <AppContext.Provider value={value}>
-            {props.children}
+            {children}
         </AppContext.Provider>
     )
 }
